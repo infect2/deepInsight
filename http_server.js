0e958ca74586216e850b763f80f27548d4b3324e
Sang Seok Lim <sangseok.lim@gmail.com>
2017-08-13 22:13:06 +0900

    web page

--- http_server.js
+++ http_server.js
@@ -1,19 +1,40 @@
-var http = require('http');
+var http = require('http'),
+    fs = require('fs');
+
+
+function serveStaticFile(res, path, contentType, responseCode) {
+  if(!responseCode) {
+    responseCode = 200;
+  }
+  fs.readFile(__dirname+path, function(err,data){
+    if(err) {
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('Internal Error');
+    } else {
+      res.writeHead(200, {'Content-Type': contentType});
+      res.end(data);
+    }
+  })
+}
 
 http.createServer(function (req, res) {
-var path = req.url.replace(/\/?(?:\?.*)?$/,'').toLowerCase();
+  //. single character except newline
+  //(?:): none capturing parentheses. 
+  //(":\?.*")?$ means ?characters one or two at the end of string
+  //in order to remove query string (? parameter). e.g) url?iam=you
+  var path = req.url.replace(/\/?(?:\?.*)?$/,'').toLowerCase();
   switch (path) {
     case '':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('Homepage');
+      serveStaticFile(res, '/public/index.html', 'text/html')
       break;
     case '/about':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('About My Homepage');
+      serveStaticFile(res, '/public/about.html', 'text/html')
+      break;
+    case '/img/survey.jpg':
+      serveStaticFile(res, '/public/img/survey.jpg', 'image/jpeg')
       break;
     default:
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.end('Not Found');    
+        serveStaticFile(res, '/public/404.html', 'text/html')
   }
 }).listen(3000, "127.0.0.1");
 
