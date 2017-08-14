b31249918bccf807788729a00df5f9e63800059f
Sang Seok Lim <sangseok.lim@gmail.com>
2017-08-15 07:34:12 +0900

    cookie and session

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var fortune = require('./lib/fortune.js');
-
+var formidable = require('formidable');
+var credentials = require('./credentials.js');
 var app = express();
 
 //view engine, or handlebars setting
@@ -65,12 +66,24 @@ app.use(function(req, res, next){
 
 //static file serving
 app.use(express.static(__dirname + '/public'));
+//
+app.use(require('body-parser').urlencoded({ extended: true }));
+//cookie signing middleware
+app.use(require('cookie-parser')(credentials.cookieSecret));
+//session setting
+app.use(require('express-session'){
+  resave: false,
+  saveUninitialized: false,
+  secret: credentials.cookieSecret
+});
 
 app.get('/', function(req, res){
+  res.cookie('sangseoklim', "handsome", { signed: true });
   res.render('home');
 });
 
 app.get('/about', function(req, res){
+  res.clearCookie('sangseoklim');
   res.render('about', {
     fortune: fortune.getFortune(),
     pageTestScript: 'qa/tests-about.js'
@@ -100,6 +113,41 @@ app.get('/data/nursery-rhyme', function(req, res){
                 adjective: 'bushy',
                 noun: 'heck',
         });
+});
+
+app.get('/thank-you', function(req, res){
+        res.render('thank-you');
+});
+app.get('/newsletter', function(req, res){
+    // we will learn about CSRF later...for now, we just 
+    // provide a dummy value
+    res.render('newsletter', { csrf: 'CSRF token goes here' });
+});
+app.post('/process', function(req, res){
+    if(req.xhr || req.accepts('json,html')==='json'){
+        // if there were an error, we would send { error: 'error description' }
+        res.send({ success: true });
+    } else {
+        // if there were an error, we would redirect to an error page
+        res.redirect(303, '/thank-you');
+    }
+});
+
+app.get('/contest/vacation-photo', function(req, res){
+    var now = new Date(); 
+    res.render('contest/vacation-photo', { year: now.getFullYear(), month: now.getMonth() });
+});
+
+app.post('/contest/vacation-photo/:year/:month', function(req, res){
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files){ 
+        if(err) return res.redirect(303, '/error');
+        console.log('received fields:');
+        console.log(fields);
+        console.log('received files:');
+        console.log(files);
+        res.redirect(303, '/thank-you');
+    });
 });
 
 app.use(function(req, res){
