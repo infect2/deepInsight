78e620e2437268d22b11b129cff21a74f84dd8bd
Sang Seok Lim <sangseok.lim@gmail.com>
2017-08-15 07:59:18 +0900

    session test code

--- app.js
+++ app.js
@@ -71,10 +71,19 @@ app.use(require('body-parser').urlencoded({ extended: true }));
 //cookie signing middleware
 app.use(require('cookie-parser')(credentials.cookieSecret));
 //session setting
-app.use(require('express-session'){
+app.use(require('express-session')({
   resave: false,
   saveUninitialized: false,
   secret: credentials.cookieSecret
+}));
+
+// flash message middleware
+app.use(function(req, res, next){
+        // if there's a flash message, transfer
+        // it to the context, then clear it
+        res.locals.flash = req.session.flash;
+        delete req.session.flash;
+        next();
 });
 
 app.get('/', function(req, res){
@@ -131,6 +140,47 @@ app.post('/process', function(req, res){
         // if there were an error, we would redirect to an error page
         res.redirect(303, '/thank-you');
     }
+});
+
+// for now, we're mocking NewsletterSignup:
+function NewsletterSignup(){
+}
+NewsletterSignup.prototype.save = function(cb){
+        cb();
+};
+
+var VALID_EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/;
+
+app.post('/newsletter', function(req, res){
+        var name = req.body.name || '', email = req.body.email || '';
+        // input validation
+        if(!email.match(VALID_EMAIL_REGEX)) {
+                if(req.xhr) return res.json({ error: 'Invalid name email address.' });
+                req.session.flash = {
+                        type: 'danger',
+                        intro: 'Validation error!',
+                        message: 'The email address you entered was  not valid.',
+                };
+                return res.redirect(303, '/newsletter/archive');
+        }
+        new NewsletterSignup({ name: name, email: email }).save(function(err){
+                if(err) {
+                        if(req.xhr) return res.json({ error: 'Database error.' });
+                        req.session.flash = {
+                                type: 'danger',
+                                intro: 'Database error!',
+                                message: 'There was a database error; please try again later.',
+                        };
+                        return res.redirect(303, '/newsletter/archive');
+                }
+                if(req.xhr) return res.json({ success: true });
+                req.session.flash = {
+                        type: 'success',
+                        intro: 'Thank you!',
+                        message: 'You have now been signed up for the newsletter.',
+                };
+                return res.redirect(303, '/newsletter/archive');
+        });
 });
 
 app.get('/contest/vacation-photo', function(req, res){
