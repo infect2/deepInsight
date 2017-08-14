1cd5e81a548ccddc1a6b51343ec73c6b70c08d91
Sang Seok Lim <sangseok.lim@gmail.com>
2017-08-14 11:28:07 +0900

    improve

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,7 @@ module.exports = function(grunt){
 			all: { src: 'qa/tests-*.js', options: { ui: 'tdd' }, }
 		},
 		jshint: {
-			app: ['app.js', 'public/js/**/*.js', 'lib/**/*.js'],
+			app: ['app.js', 'public/js/*.js', 'lib/**/*.js'],
 			qa: ['Gruntfile.js', 'public/qa/**/*.js', 'qa/**/*.js'],
 		},
 		exec: {
