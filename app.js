fbe4230cdd6ca096bf22e5956d16a72f12fc01c9
Sang Seok Lim <sangseok.lim@gmail.com>
2017-08-14 11:26:51 +0900

    improve

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 var express = require('express');
-var fortune = require('./lib/fortune.js')
+var fortune = require('./lib/fortune.js');
 
 var app = express();
 
 //view engine, or handlebars setting
-var handlebars = require('express-handlebars').create({ defaultLayout: 'main'});
+var handlebars = require('express-handlebars').create({
+  defaultLayout: 'main',
+  helpers:{
+    section: function(name, options){
+    if(!this._sections) this._sections = {};
+    this._sections[name] = options.fn(this);
+    return null;
+    }
+  }
+});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine','handlebars');
 
@@ -14,12 +23,6 @@ app.set('port', process.env.PORT || 3000);
 app.use(function(req, res, next){
   res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
   next();
-});
-
-app.use(function(err, req,res, next){
-  console.error(err, stack);
-  res.status(500);
-  res.render('500');
 });
 
 //static file serving
@@ -51,6 +54,12 @@ app.get('/tours/request-group-rate', function(req, res){
 app.use(function(req, res){
   res.status(404);
   res.render('404');
+});
+
+app.use(function(err, req,res, next){
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
 });
 
 app.listen(app.get('port'), function(){
