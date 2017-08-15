8e3a293f77f5a7fd588ad0adfbd32387f90d6f9a
Sang Seok Lim <sangseok.lim@gmail.com>
2017-08-15 12:30:28 +0900

    error is notified by email

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var credentials = require('./credentials.js');
 var connect = require('connect');
 var compression = require('compression');
 var email = require('./lib/email.js');
+var emailService = email(credentials);
 
 var app = express();
 
@@ -216,17 +217,16 @@ app.post('/contest/vacation-photo/:year/:month', function(req, res){
 app.use(function(req, res){
   res.status(404);
   res.render('404');
+  emailService.send('infect2@hanmail.net', 'Service Alert', '404 Not Found');
 });
 
 app.use(function(err, req,res, next){
   console.error(err.stack);
   res.status(500);
   res.render('500');
+  emailService.send('infect2@hanmail.net', 'Service Alert', 'Internal Server Error');
 });
 
 app.listen(app.get('port'), function(){
   console.log('Express started on http://localhost' + app.get('port'));
 });
-
-var transport = email(credentials);
-transport.send('infect2@hanmail.net', 'Service Alert', 'Express Server just Started');
