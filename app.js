eb8daca1f5e989275fae647a526aea7aa0d82fc7
Sang Seok Lim <sangseok.lim@gmail.com>
2017-08-15 13:47:23 +0900

    cluster for multi-CPU scaling

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var http = require('http');
 var express = require('express');
 var fortune = require('./lib/fortune.js');
 var formidable = require('formidable');
@@ -24,6 +25,18 @@ app.engine('handlebars', handlebars.engine);
 app.set('view engine','handlebars');
 
 app.set('port', process.env.PORT || 3000);
+
+//logger setting
+switch(app.get('env')){
+  case 'development':
+    app.use(require('morgan')('dev'));
+    break;
+  case 'production':
+    app.use(require('express-logger')({
+      path: __dirname + '/log/requests.log'
+    }));
+    break;
+}
 
 //test page support. it should be placed in front of other routers
 app.use(function(req, res, next){
@@ -95,7 +108,10 @@ app.use(function(req, res, next){
 });
 
 app.use(function(req, res, next){
-  console.log('processing request for ' + req.url + '...');
+  var cluster = require('cluster');
+  if(cluster.isWorker) {
+    console.log('Worker %d processing request for %s', cluster.worker.id, req.url);
+  }
   next();
 });
 
@@ -227,6 +243,26 @@ app.use(function(err, req,res, next){
   emailService.send('infect2@hanmail.net', 'Service Alert', 'Internal Server Error');
 });
 
-app.listen(app.get('port'), function(){
-  console.log('Express started on http://localhost' + app.get('port'));
-});
+
+var server;
+
+function startServer() {
+    server = http.createServer(app).listen(app.get('port'), function(){
+      console.log( 'Express started in ' + app.get('env') +
+        ' mode on http://localhost:' + app.get('port') +
+        '; press Ctrl-C to terminate.' );
+    });
+}
+
+if(require.main === module){
+    // application run directly; start app server
+    startServer();
+} else {
+    // application imported as a module via "require": export function to create server
+    module.exports = startServer;
+}
+
+// app.listen(app.get('port'), function(){
+//   console.log('Express started on http://localhost' + app.get('port'));
+//   console.log("Execution Mode: " + app.get('env'));
+// });
